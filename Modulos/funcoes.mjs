928b54af7fb9703daae014518af38f10e37be79c
ORLANDO DOS SANTOS DOURADO <62121619+OrlandoSD@users.noreply.github.com>
2022-03-21 17:41:50 -0300

    Add files via upload

---
+++ Modulos/funcoes.mjs
@@ -0,0 +1,13 @@
+function mostrarIdeade(nome, idade){
+    return ´A idade de &{nome} é &{idade}.´;
+}
+
+function mostrarCidede(nome, cidade){
+    return ´A cidade de &{nome} é &{cidade}.´;
+}
+
+function mostrarHobby(nome, hobby){
+    return ´O hobby de &{nome} é &{hobby}.´;
+}
+
+export {mostrarIdade, mostrarCidade, mostrarHobby};
---
+++ Modulos/main.mjs
@@ -0,0 +1,5 @@
+import{mostrarCidade, mostrarIdade, mostrarHobby} from './funcoes.mjs';
+
+console.log(mostrarIdade("fulado de tal", 28));
+console.log(mostrarCidade("Fulano de tal", "São Paulo"));
+console.log(mostrarHobby("Fulano de tal", "Patinar"));
